8888a6d57a5320146307657063ce1e4981040ed2
Julianmotta07 <mottajulian47@gmail.com>
2025-09-28 18:19:09 -0500

    ci(terraform): add github actions pipeline

--- backend.tf
+++ backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatejm07"
-    container_name       = "tfstate"
-    key                  = "infra.terraform.tfstate"
-  }
+  backend "azurerm" {}
 }
